80e36ddd482d4ef2ce51ce4f4e087bbfc657f64b
lueping <107954948+lueping@users.noreply.github.com>
2022-06-22 11:44:23 -0700

    Add files via upload

--- hai.R
+++ hai.R
@@ -15,7 +15,7 @@ require(stringdist); require(irr); require(xlsx);require(limma);require(pbapply)
 # save(metadata, file="data/pred_data_2022_05_18.Rdata")
 
 # Loading data
-load(file="data/preddata_2022_05_18_part.Rdata")
+load(file="data/preddata_2022_05_18_full.Rdata")
 load(file="data/ref seq.Rdata"); rownames(ref_AA)=sub("-","_", rownames(ref_AA))
 load(file="data/hap cores.Rdata")
 
