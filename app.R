382f9836c39e53c2f4b3b5edd64473ac7c55d9e3
lueping <107954948+lueping@users.noreply.github.com>
2022-06-23 21:51:44 -0700

    Update app.R

--- app.R
+++ app.R
@@ -19,7 +19,7 @@ ui <- fluidPage(theme = shinytheme("united"),shinyjs::useShinyjs(),
 								),
 								
 								headerPanel('AI for SARS-COV-2 Variant Predictions'),
-								tags$a("Term of Use",target="_blank",href="./HAI_TOU.pdf"),
+								a("Term of Use",target="_blank",href="./HAI_TOU.pdf"),
 								
 								# Input values
 								sidebarPanel(
